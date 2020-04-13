3b82853557a06d400323bab1f1ef50f3d35f0097
herve <mugunga5@gmail.com>
2020-04-13 23:42:58 +0200

    add another bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,6 +11,24 @@ def bubble_sort(arr)
   end
   arr
 end
-
-
 puts bubble_sort([5,7,3,1,6,7])
+
+def bubble_sort_by(arr)
+  swap = false
+  until swap do
+    swap = true
+    for i in 0...(arr.length-1)
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swap = false
+      end
+    end
+  end
+  yield
+  
+end
+
+bubble_sort_by(&array) { 
+  array = ["hello","hi","ruby"]
+  puts array
+}
