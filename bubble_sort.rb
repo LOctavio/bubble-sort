2664330243901719979e8df8a5b381c6c035dadc
LOctavio <luis_ram1997@hotmail.com>
2020-04-13 15:45:14 -0500

    complete firstbubble sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,16 @@
 def bubble_sort(arr)
   swap = false
   until swap do
-  for i in 0...(arr.length-1)
-    swap = false
-    if arr[i] > arr[i+1]
-      arr[i], arr[i+1] = arr[i+1], arr[i]
-      swap = true
-    else
-      break
+    swap = true
+    for i in 0...(arr.length-1)
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swap = false
+      end
     end
   end
-end
-
   arr
 end
 
 
-puts bubble_sort([3,2,1])
+puts bubble_sort([5,7,3,1,6,7])
