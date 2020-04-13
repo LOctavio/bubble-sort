a702a4d8c1d2593d71b86ff3483cdcd0892cc39e
herve <mugunga5@gmail.com>
2020-04-13 22:12:57 +0200

    Add until loop incomplete

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,17 @@
 def bubble_sort(arr)
+  swap = false
+  until swap do
   for i in 0...(arr.length-1)
+    swap = false
     if arr[i] > arr[i+1]
       arr[i], arr[i+1] = arr[i+1], arr[i]
+      swap = true
+    else
+      break
     end
   end
+end
+
   arr
 end
 
