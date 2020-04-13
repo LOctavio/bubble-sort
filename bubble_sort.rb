5d4b604051741e484d415318bae6a24d52b5090f
LOctavio <luis_ram1997@hotmail.com>
2020-04-13 17:02:43 -0500

    add block with spaceship operator and call block inside bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,23 +12,20 @@ def bubble_sort(arr)
   arr
 end
 puts bubble_sort([5,7,3,1,6,7])
+puts ""
 
 def bubble_sort_by(arr)
   swap = false
   until swap do
     swap = true
     for i in 0...(arr.length-1)
-      if arr[i] > arr[i+1]
+      if yield(arr[i],arr[i+1]) > 0
         arr[i], arr[i+1] = arr[i+1], arr[i]
         swap = false
       end
     end
   end
-  yield
-  
+  puts arr
 end
 
-bubble_sort_by(&array) { 
-  array = ["hello","hi","ruby"]
-  puts array
-}
+bubble_sort_by(["hello","hi","ruby"]) { |right, left| right.length <=> left.length}
