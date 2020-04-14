d3cbe1eb0c990230cda6ed50a622b93db312b9e5
herve <mugunga5@gmail.com>
2020-04-14 16:18:24 +0200

    fix lint errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,25 @@
 def bubble_sort(arr)
   swap = false
-  until swap do
+  until swap
     swap = true
-    for i in 0...(arr.length-1)
-      if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (0...(arr.length - 1)).each do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swap = false
       end
     end
   end
   arr
 end
-puts bubble_sort([5,7,3,1,6,7])
-puts ""
+puts bubble_sort([5, 7, 3, 1, 6, 7])
+puts ''
 def bubble_sort_by(arr)
   swap = false
-  until swap do
+  until swap
     swap = true
-    for i in 0...(arr.length-1)
-      if yield(arr[i],arr[i+1]) > 0
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (0...(arr.length - 1)).each do |i|
+      if yield(arr[i], arr[i + 1]) > 0
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swap = false
       end
     end
@@ -27,4 +27,4 @@ def bubble_sort_by(arr)
   puts arr
 end
 
-bubble_sort_by(["hello","hi","ruby"]) { |right, left| right.length <=> left.length}
+bubble_sort_by(%w[hello hi ruby]) { |right, left| right.length <=> left.length }
